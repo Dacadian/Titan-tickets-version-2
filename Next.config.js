878568f4d0c5cbdadd7014d6fdf67c6c18121100
Dacadian <jammalradrex7@gmail.com>
2026-04-28 22:23:28 +0300

    Update Next.config.js

--- Next.config.js
+++ Next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
+module.exports = {
+  experimental: { appDir: true }
 }
-
-module.exports = nextConfig
